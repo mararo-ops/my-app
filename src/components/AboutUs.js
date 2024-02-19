7e036aea9d67d01c058e824be473b661e14264c9
mararo-ops <danielmararo1@gmail.com>
2024-02-20 00:27:02 +0300

    final commit

---
+++ src/components/AboutUs.js
@@ -0,0 +1,255 @@
+import React from "react";
+import styles from '../App.css'; // Use appropriate relative path
+
+
+const Component1 = () => {
+  return (
+    <div className={styles.component1}>
+      <div className={styles.frame7}>
+        <p className={styles.whyChriscoCentral}>
+          Why Chrisco Central?
+        </p>
+        <p
+          className={
+            styles.theCentralChurchIsACongregationUnd
+          }
+        >
+          The Central Church is a congregation under the{" "}
+          <br />
+          larger Christ’s co-Workers Fellowship (Chrisco)
+          situated in Woodley in Nairobi, <br />
+          Kenya.
+          <br />
+          The Central Church is headed by Presbyter Jeremiah
+          Mugala and Pastor Rosemary Mugala together with
+          other elder-ship.{" "}
+        </p>
+        <div className={styles.relativeWrapperOne}>
+          <img
+            alt=""
+            className={styles.image32}
+            src="https://static.overlay-tech.com/assets/6fd7c465-8446-44a1-a27b-c8bc573c590f.png"
+          />
+          <div className={styles.flexWrapperOne}>
+            <p
+              className={
+                styles.theChurchAimsAtMeetingTheNeedsOf
+              }
+            >
+              The Church aims at meeting the needs of the
+              total man spiritually.
+              <br />
+              Our dedicated team of elder-ship and
+              volunteers work tirelessly to create warm and{" "}
+              <br />
+              conducive environment for all.{" "}
+            </p>
+            <div className={styles.frame19}>
+              <p className={styles.exploreMore}>
+                Explore More
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.frame13}>
+        <div className={styles.missionTwo}>
+          <div className={styles.mission}>
+            <p className={styles.ourMission}>Our Mission</p>
+            <p
+              className={
+                styles.toEquipBelieversThroughPrayerTeach
+              }
+            >
+              To equip believers through prayer, teaching
+              God’s word, discipleship, evangelism,
+              Christian living, tent-making so that they can
+              be united, and attain the full knowledge of
+              Christ in spiritual maturity
+            </p>
+          </div>
+        </div>
+        <div className={styles.vision}>
+          <div className={styles.mission}>
+            <p className={styles.ourVision}>Our Vision</p>
+            <p
+              className={
+                styles.toBeACongregationOfBelieversDiscip
+              }
+            >
+              To be a congregation of believers disciples
+              under the five-fold ministry to fulfill the
+              great commission of our Lord Jesus Christ.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.relativeWrapperTwo}>
+        <div className={styles.word}>
+          <div className={styles.flexWrapperTwo}>
+            <img
+              alt=""
+              className={styles.image24}
+              src="https://static.overlay-tech.com/assets/b33a4b6e-f3d0-4b7d-bccf-029ea3a860ac.png"
+            />
+            <div className={styles.group19}>
+              <p className={styles.theWord}>THE WORD</p>
+              <p
+                className={
+                  styles.loremIpsumDolorSitAmetConsectetur
+                }
+              >
+                Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Sed in mauris congue,
+                dignissim nulla et, tincidunt velit. Sed
+                libero arcu, convallis in eros vel, egestas
+                congue nulla. Sed nec dictum nulla. Nulla
+                facilisi. Aliquam erat volutpat. Sed non
+                quam arcu. Donec euismod mauris.
+              </p>
+            </div>
+          </div>
+          <div className={styles.line1Stroke} />
+        </div>
+        <div className={styles.wordTwo}>
+          <div className={styles.flexWrapperThree}>
+            <div className={styles.group19Two}>
+              <p className={styles.ourFaith}>OUR FAITH</p>
+              <p
+                className={
+                  styles.atChriscoCentralChurchWeOfferAVa
+                }
+              >
+                At Chrisco Central church, We offer a
+                variety of services and programs to meet the
+                spiritual needs of our congregation. Our
+                Sunday services include uplifting music,
+                inspiring sermons, and opportunities for
+                personal growth and connection. We also have
+                programs for children, youth, and adults
+                throughout the week.
+              </p>
+            </div>
+            <img
+              alt=""
+              className={styles.image53}
+              src="https://static.overlay-tech.com/assets/b4132adc-3a67-4f4e-9ee4-41080b466650.png"
+            />
+          </div>
+          <div className={styles.line1Stroke} />
+        </div>
+      </div>
+      <div className={styles.aboutUs}>
+        <div className={styles.frame1}>
+          <div className={styles.group1}>
+            <div className={styles.image9} />
+            <div className={styles.flexWrapperFour}>
+              <img
+                alt=""
+                className={styles.photo}
+                src="https://static.overlay-tech.com/assets/1e4b65a3-db95-46bd-aa93-2ad9e41a50f4.png"
+              />
+              <p className={styles.ourChurch}>OUR CHURCH</p>
+            </div>
+            <p
+              className={
+                styles.loremIpsumIsSimplyDummyTextOfThe
+              }
+            >
+              Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry. Lorem Ipsum
+              has been the industry&#039;s standard dummy
+              text ever since the 1500s.Lorem Ipsum is
+              simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum has been the
+              industry&#039;s standard dummy text ever since
+              the 1500s.
+            </p>
+          </div>
+          <div className={styles.group2}>
+            <div className={styles.image9Two} />
+            <div className={styles.flexWrapperFive}>
+              <img
+                alt=""
+                className={styles.photoTwo}
+                src="https://static.overlay-tech.com/assets/97604107-d330-45fa-95a0-a7eb526a4165.png"
+              />
+              <p className={styles.ourPurpose}>
+                OUR PURPOSE
+              </p>
+            </div>
+            <p
+              className={
+                styles.loremIpsumIsSimplyDummyTextOfTheTwo
+              }
+            >
+              Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry. Lorem Ipsum
+              has been the industry&#039;s standard dummy
+              text ever since the 1500s.Lorem Ipsum is
+              simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum has been the
+              industry&#039;s standard dummy text ever since
+              the 1500s.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.relativeWrapperThree}>
+        <div className={styles.baptism}>
+          <img
+            alt=""
+            className={styles.image26}
+            src="https://static.overlay-tech.com/assets/2727d868-318f-476d-98dc-1dcea0553291.png"
+          />
+          <div className={styles.group19Three}>
+            <p className={styles.theBaptism}>THE BAPTISM</p>
+            <p
+              className={
+                styles.loremIpsumDolorSitAmetConsecteturTwo
+              }
+            >
+              Lorem ipsum dolor sit amet, consectetur
+              adipiscing elit. Sed in mauris congue,
+              dignissim nulla et, tincidunt velit. Sed
+              libero arcu, convallis in eros vel, egestas
+              congue nulla. Sed nec dictum nulla. Nulla
+              facilisi. Aliquam erat volutpat. Sed non quam
+              arcu. Donec euismod mauris.
+            </p>
+          </div>
+        </div>
+        <div className={styles.trinity}>
+          <div className={styles.flexWrapperSix}>
+            <div className={styles.group19Four}>
+              <p className={styles.theTrinity}>
+                THE TRINITY
+              </p>
+              <p
+                className={
+                  styles.loremIpsumDolorSitAmetConsecteturThree
+                }
+              >
+                Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit. Sed in mauris congue,
+                dignissim nulla et, tincidunt velit. Sed
+                libero arcu, convallis in eros vel, egestas
+                congue nulla. Sed nec dictum nulla. Nulla
+                facilisi. Aliquam erat volutpat. Sed non
+                quam arcu. Donec euismod mauris.
+              </p>
+            </div>
+            <img
+              alt=""
+              className={styles.image25}
+              src="https://static.overlay-tech.com/assets/97be1b55-482b-4fb3-9243-3e11fa6d29ac.png"
+            />
+          </div>
+          <div className={styles.line1Stroke} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Component1;
